036ec395a2c410a88c269c72bbbf1fcf7a583113
Md Shamim Hasan <mdshamimhasan791@gmail.com>
2026-02-22 23:19:54 +0600

    rejected funtionality added

--- js/script.js
+++ js/script.js
@@ -64,6 +64,15 @@ if (interviewCountList.length <= 0) {
   </div>
   `;
 }
+if (rejectedCountList.length <= 0) {
+  filterdContainer.innerHTML = `
+  <div class="flex flex-col items-center gap-4">
+  <img src="./img/jobs.png">
+  <h2 class="font-bold text-3xl text-[#002C5C]">No Jobs Available</h2>
+  <p class="font-bold text-black">Check back soon for new job opportunities</p>
+  </div>
+  `;
+}
 
 // find main container and addEventListener
 const mainContainer = document.querySelector("main");
@@ -77,7 +86,7 @@ mainContainer.addEventListener("click", (event) => {
     const status = parentNode.querySelector(".status-1").innerText;
     const jobReview = parentNode.querySelector(".job-info").innerText;
 
-    parentNode.querySelector(".status-1").innerText = "APPLIED";
+    parentNode.querySelector(".status-1").innerText = "INTERVIEW";
     parentNode
       .querySelector(".status-1")
       .classList.add("bg-green-500", "text-white");
@@ -86,11 +95,10 @@ mainContainer.addEventListener("click", (event) => {
       companyName,
       position,
       jobDetails,
-      status: "APPLIED",
+      status: "INTERVIEW",
       jobReview,
     };
     // console.log(jobInfo);
-
     let interviewListExist = interviewCountList.find(
       (item) => item.companyName === jobInfo.companyName
     );
@@ -100,6 +108,33 @@ mainContainer.addEventListener("click", (event) => {
     }
     count();
     interviewRender();
+  } else if (event.target.classList.contains("rejected")) {
+    const parentNode = event.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector(".company").innerText;
+    const position = parentNode.querySelector(".position").innerText;
+    const jobDetails = parentNode.querySelector(".job-detaills").innerText;
+    const status = parentNode.querySelector(".status-1").innerText;
+    const jobReview = parentNode.querySelector(".job-info").innerText;
+    parentNode.querySelector(".status-1").innerText = "REJECTED";
+    parentNode
+      .querySelector(".status-1")
+      .classList.add("bg-red-500", "text-white");
+    const jobInfo = {
+      companyName,
+      position,
+      jobDetails,
+      status: "REJECTED",
+      jobReview,
+    };
+    let rejectedListExist = interviewCountList.find(
+      (item) => item.companyName === jobInfo.companyName
+    );
+
+    if (!rejectedListExist) {
+      rejectedCountList.push(jobInfo);
+    }
+    count();
+    rejectedRender();
   }
 });
 
@@ -116,6 +151,30 @@ function interviewRender() {
                   <p class="job-detaills">${interview.jobDetails}</p>
                   <span class="status-1 bg-gray-300 text-[#002C5C] p-2 inline-block my-2">${interview.status}</span>
                   <p class="job-info">${interview.jobReview}</p>
+                  <div>
+                    <button  class="interview btn btn-primary btn-soft">Interview</button>
+                    <button class="rejected btn btn-error btn-soft">Rejected</button>
+                  </div>
+                </div>
+                <div><button class=" border-amber-800 px-2 text-red-500"><i class="fa-solid fa-trash-can"></i></button></div>
+  `;
+    filterdContainer.appendChild(div);
+  }
+}
+
+function rejectedRender() {
+  filterdContainer.innerHTML = ``;
+  for (let rejected of rejectedCountList) {
+    const div = document.createElement("div");
+    div.className =
+      "p-6 hover:shadow shadow-blue-500 duration-300 flex justify-between";
+    div.innerHTML = `
+   <div class="space-y-3">
+                  <h2 class="company font-bold text-[#002C5C] text-2xl">${rejected.companyName}</h2>
+                  <p class="position font-semibold text-gray-500">${rejected.position}</p>
+                  <p class="job-detaills">${rejected.jobDetails}</p>
+                  <span class="status-1 bg-gray-300 text-[#002C5C] p-2 inline-block my-2">${rejected.status}</span>
+                  <p class="job-info">${rejected.jobReview}</p>
                   <div>
                     <button  class="interview btn btn-primary btn-soft">Interview</button>
                     <button class="rejected btn btn-error btn-soft">Rejected</button>
