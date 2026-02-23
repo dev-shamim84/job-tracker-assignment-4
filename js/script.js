32e184abf6be73c09d4955c0e60dbfbb20664945
Md Shamim Hasan <mdshamimhasan791@gmail.com>
2026-02-23 15:49:06 +0600

    data toggle funtionality added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let interviewCountList = [];
 let rejectedCountList = [];
+let currentStatus = "all";
 
 const total = document.getElementById("total");
 const interview = document.getElementById("interview");
@@ -24,6 +25,7 @@ const rejectedBtn = document.getElementById("rejected-filter-btn");
 const allcardsSection = document.getElementById("allcards-section");
 const filterdSection = document.getElementById("filterd-section");
 
+const of = document.getElementById("of");
 // toggle button
 function toggleStyle(id) {
   // add color onclick
@@ -35,45 +37,32 @@ function toggleStyle(id) {
   allBtn.classList.remove("bg-blue-500", "text-white");
   interviewBtn.classList.remove("bg-blue-500", "text-white");
   rejectedBtn.classList.remove("bg-blue-500", "text-white");
+  console.log(interviewCountList.length, rejectedCountList.length);
+
+  // set id
+  const selected = document.getElementById(id);
+  // currentStatus(id);
+
+  selected.classList.add("bg-blue-500", "text-white");
+  selected.classList.remove("bg-gray-300", "text-black");
 
   // filter by category job
   if (id === "all-filter-btn") {
+    of.innerText = "";
     allcardsSection.classList.remove("hidden");
     filterdSection.classList.add("hidden");
   } else if (id === "interview-filter-btn") {
+    of.innerText = `${interviewCountList.length} of `;
     filterdSection.classList.remove("hidden");
     allcardsSection.classList.add("hidden");
+    interviewRender();
   } else if (id === "rejected-filter-btn") {
-    allcardsSection.classList.add("hidden");
+    of.innerText = `${rejectedCountList.length} of `;
     filterdSection.classList.remove("hidden");
+    allcardsSection.classList.add("hidden");
+    rejectedRender();
   }
-
-  const selected = document.getElementById(id);
-  selected.classList.add("bg-blue-500", "text-white");
-  selected.classList.remove("bg-gray-300", "text-black");
 }
-
-// conditional render by fitersection
-const filterdContainer = document.getElementById("filter-container");
-if (interviewCountList.length <= 0) {
-  filterdContainer.innerHTML = `
-  <div class="flex flex-col items-center gap-4">
-  <img src="./img/jobs.png">
-  <h2 class="font-bold text-3xl text-[#002C5C]">No Jobs Available</h2>
-  <p class="font-bold text-black">Check back soon for new job opportunities</p>
-  </div>
-  `;
-}
-if (rejectedCountList.length <= 0) {
-  filterdContainer.innerHTML = `
-  <div class="flex flex-col items-center gap-4">
-  <img src="./img/jobs.png">
-  <h2 class="font-bold text-3xl text-[#002C5C]">No Jobs Available</h2>
-  <p class="font-bold text-black">Check back soon for new job opportunities</p>
-  </div>
-  `;
-}
-
 // find main container and addEventListener
 const mainContainer = document.querySelector("main");
 mainContainer.addEventListener("click", (event) => {
@@ -106,8 +95,12 @@ mainContainer.addEventListener("click", (event) => {
     if (!interviewListExist) {
       interviewCountList.push(jobInfo);
     }
-    count();
+    rejectedCountList = rejectedCountList.filter(
+      (item) => item.companyName !== jobInfo.companyName
+    );
     interviewRender();
+    rejectedRender();
+    count();
   } else if (event.target.classList.contains("rejected")) {
     const parentNode = event.target.parentNode.parentNode;
     const companyName = parentNode.querySelector(".company").innerText;
@@ -126,20 +119,35 @@ mainContainer.addEventListener("click", (event) => {
       status: "REJECTED",
       jobReview,
     };
-    let rejectedListExist = interviewCountList.find(
+    let rejectedListExist = rejectedCountList.find(
       (item) => item.companyName === jobInfo.companyName
     );
 
     if (!rejectedListExist) {
       rejectedCountList.push(jobInfo);
     }
-    count();
+    console.log(interviewCountList);
+    interviewCountList = interviewCountList.filter(
+      (item) => item.companyName !== jobInfo.companyName
+    );
+    console.log(interviewCountList);
     rejectedRender();
+    interviewRender();
+    count();
   }
 });
-
+const filterdContainer = document.getElementById("filter-container");
 function interviewRender() {
   filterdContainer.innerHTML = ``;
+  if (interviewCountList.length <= 0) {
+    filterdContainer.innerHTML = `
+  <div class="flex flex-col items-center gap-4">
+  <img src="./img/jobs.png">
+  <h2 class="font-bold text-3xl text-[#002C5C]">No Jobs Available</h2>
+  <p class="font-bold text-black">Check back soon for new job opportunities</p>
+  </div>
+  `;
+  }
   for (let interview of interviewCountList) {
     const div = document.createElement("div");
     div.className =
@@ -161,14 +169,23 @@ function interviewRender() {
     filterdContainer.appendChild(div);
   }
 }
-
 function rejectedRender() {
   filterdContainer.innerHTML = ``;
-  for (let rejected of rejectedCountList) {
-    const div = document.createElement("div");
-    div.className =
-      "p-6 hover:shadow shadow-blue-500 duration-300 flex justify-between";
-    div.innerHTML = `
+  console.log(rejectedCountList.length);
+  if (rejectedCountList.length <= 0) {
+    filterdContainer.innerHTML = `
+  <div class="flex flex-col items-center gap-4">
+  <img src="./img/jobs.png">
+  <h2 class="font-bold text-3xl text-[#002C5C]">No Jobs Available</h2>
+  <p class="font-bold text-black">Check back soon for new job opportunities</p>
+  </div>
+  `;
+  } else {
+    for (let rejected of rejectedCountList) {
+      const div = document.createElement("div");
+      div.className =
+        "p-6 hover:shadow shadow-blue-500 duration-300 flex justify-between";
+      div.innerHTML = `
    <div class="space-y-3">
                   <h2 class="company font-bold text-[#002C5C] text-2xl">${rejected.companyName}</h2>
                   <p class="position font-semibold text-gray-500">${rejected.position}</p>
@@ -182,6 +199,7 @@ function rejectedRender() {
                 </div>
                 <div><button class=" border-amber-800 px-2 text-red-500"><i class="fa-solid fa-trash-can"></i></button></div>
   `;
-    filterdContainer.appendChild(div);
+      filterdContainer.appendChild(div);
+    }
   }
 }
